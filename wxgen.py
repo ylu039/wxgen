58986fddac05c78b04b40776bd896e9d263287ac
Yaqiong Lu <yaqiong@cheyenne4.ib0.cheyenne.ucar.edu>
2018-04-18 10:20:48 -0600

    Add dDenver
    To address #5

--- wxgen.py
+++ wxgen.py
@@ -13,7 +13,7 @@ from random import gauss, random
 from datetime import datetime
 
 # User settings
-city_list   = ["boulder","philadelphia"]
+city_list   = ["boulder","denver","philadelphia"]
 fcst_path   = "forecasts/{}.txt"
 num_days    = 7
 
